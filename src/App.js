31d6032d561909cb54d7f78f0e884529beb9f8a7
Dheeraj <marumojudheeraj@gmail.com>
2023-12-31 19:29:58 +0530

    Interaction done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
-import Login from './Components/Login';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Components/Login";
+import { getTokenFromUrl } from "./Components/Spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log("User:", user);
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Login />
+      {token ? (
+        <h1>I am logged in</h1> // <Player />
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
